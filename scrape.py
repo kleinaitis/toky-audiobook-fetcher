cdfd040dc8b8f320da3b16b6a09c2123e78c817b
Jeff <jeffkleinaitis@gmail.com>
2023-12-13 17:17:58 -0500

    refactor: Decompose large scraping and formatting function into smaller, modular components

--- scrape.py
+++ scrape.py
@@ -3,29 +3,35 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote, urljoin
 import re
 
-links_array = []
-
 
 def get_audiobook_title(url):
     return url.rsplit("/", 1)[1].replace('-', ' ').title()
 
 
-def get_audiobook_download_links(URL):
-    global links_array
-    URL = URL
-    page = requests.get(URL)
-
-    soup = BeautifulSoup(page.content, "html.parser")
-    script_tags = soup.find_all("script")
-
+def fetch_audiobook_script_content(audiobook_url):
     # Tokybook stores the "tracks" object in the 19th script tag on every audiobook page
-    target_index = 18
+    target_script_index = 18
+    try:
+        page = requests.get(audiobook_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        script_tags = soup.find_all("script")
 
-    if 0 <= target_index < len(script_tags):
-        script_content = script_tags[target_index].string
-        pattern = re.compile(r'"chapter_link_dropbox"\s*:\s*"(.*?)",')
+        if 0 <= target_script_index < len(script_tags):
+            script_content = script_tags[target_script_index].string
+            return script_content
+    except requests.RequestException as e:
+        print(f"Error fetching audiobook script content: {e}")
+        return None
 
-        matches = re.findall(pattern, script_content)
+
+def extract_and_format_download_links(audiobook_url):
+    script_content = fetch_audiobook_script_content(audiobook_url)
+
+    if script_content:
+        link_pattern = re.compile(r'"chapter_link_dropbox"\s*:\s*"(.*?)",')
+        download_links = []
+
+        matches = re.findall(link_pattern, script_content)
 
         for match in matches:
             # Replaces spaces with %20 in the filename
@@ -36,9 +42,7 @@ def get_audiobook_download_links(URL):
 
             full_link = urljoin("https://files02.tokybook.com/audio/", formatted_link)
 
-            links_array.append(f"{full_link}")
-
+            download_links.append(f"{full_link}")
+        return download_links
     else:
-        print(f"Script tag at index {target_index} not found.")
-
-    return links_array
+        return []
