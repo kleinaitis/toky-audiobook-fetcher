54fa01fa98dcf30b5e7ed2a575e056eb65350ac2
Jeff <jeffkleinaitis@gmail.com>
2023-12-12 21:37:35 -0500

    refactor: Refactored code for downloading and creating audiobook files into separate functions

--- download.py
+++ download.py
@@ -1,16 +1,17 @@
 import requests
 
 
-def download_and_create_chapters(url_list):
-    chapter_counter = 1
+def create_audiobook_file(response, chapter_counter):
+    file_name = f"Chapter{chapter_counter}.mp3"
+    with open(file_name, 'wb') as output_file:
+        output_file.write(response.content)
 
+
+def download_audiobook_data(audiobook_links):
     # Skip the first link since it's an advertisement
-    download_links = url_list[1:]
+    download_links = audiobook_links[1:]
 
-    for download_link in download_links:
-        file_name = f"Chapter{chapter_counter}.mp3"
-        audiobook_data = requests.get(download_link, allow_redirects=True)
+    for chapter_counter, download_link in enumerate(download_links, start=1):
+        response = requests.get(download_link, allow_redirects=True)
+        create_audiobook_file(response, chapter_counter)
 
-        with open(file_name, 'wb') as output_file:
-            output_file.write(audiobook_data.content)
-            chapter_counter += 1
