2b2505e3fd70399e4eca2d36b573890f2f19bdda
Jeff <jeffkleinaitis@gmail.com>
2023-12-15 16:42:10 -0500

    feat: Implement function to facilitate package execution as script

--- tokybook_audiobook_fetcher/main.py
+++ tokybook_audiobook_fetcher/main.py
@@ -1,6 +1,11 @@
 import sys
 
-from download import download_audiobook
+from tokybook_audiobook_fetcher.download import download_audiobook
+
+
+def main_function():
+    download_audiobook(sys.argv[1])
+
 
 if __name__ == "__main__":
     download_audiobook(sys.argv[1])
